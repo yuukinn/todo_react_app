14af97b1c981abf967d852107e70d0fcf13a6828
yuuki <87259129+yuukinn@users.noreply.github.com>
2021-09-29 00:39:56 +0900

    Create Footer.js

---
+++ src/compornent/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box } from "@mui/system";
+import { makeStyles } from  "@mui/styles";
+
+
+const useStyles = makeStyles(() =>  ({
+    root: {
+        width:"100%",
+        height: 56,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#FFF",
+        backgroundColor: "#3f51b5",
+        position: "fixed",
+        bottom: 0,
+    },
+}))
+
+const Footer = () => {
+    const classes = useStyles(); 
+    return(
+        <Box className={classes.root}>copyright yuuki</Box>
+    )
+}
+
+
+
+export default Footer
